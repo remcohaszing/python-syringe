e025f4326ed2b31794b3f7629ff5b9aa08662ebd
Remco Haszing <remcohaszing@gmail.com>
2014-05-23 23:39:21 +0200

    Bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('README.rst') as f:
 
 setup(
     name='syringe',
-    version='0.1.0',
+    version='0.1.1',
     author='Remco Haszing',
     author_email='remcohaszing@gmail.com',
     url='https://github.com/remcohaszing/python-syringe',
